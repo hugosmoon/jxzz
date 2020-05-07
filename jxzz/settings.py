82abe09d36254e63f60c38f7eb4c2801732fa686
Sebastian 黄 <andforever@126.com>
2020-05-07 18:26:39 +0800

    增加允许访问的ip

--- jxzz/settings.py
+++ jxzz/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%1+n8lcuhw*mmqy#8@^7w6b+lar=#g-uewr36847td4flkl2)x'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
