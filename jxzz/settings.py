97cb58c2b85f5303f3f6ddf3d7696397a92f791b
Sebastian 黄 <andforever@126.com>
2020-05-08 14:30:29 +0800

    修改static路径

--- jxzz/settings.py
+++ jxzz/settings.py
@@ -135,7 +135,7 @@ USE_L10N = True
 # )
 
 
-STATIC_URL = '/static/'
+STATIC_URL = '/jxzz_2/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
